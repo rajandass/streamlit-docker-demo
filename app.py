5d538ab3005386eb2ef584c5ba6750a2fa8718f5
rajandass <rajandass65@gmail.com>
2025-04-09 17:34:22 +0530

    Intial commit: Simple Streamlit app with Docker

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Simple Streamlit App")
+
+st.write("## Data Visualization Demo")
+
+chart_data = pd.DataFrame(
+    np.random.randn(20,3),
+    columns=['A','B','C'])
+
+st.line_chart(chart_data)
+
+number = st.slider('Select a number', 0, 100, 50)
+st.write(f'You selected: {number}')
+
+user_input = st.text_input('Enter some text')
+
+if user_input:
+    st.write(f'You entered: {user_input}')
+
+    
+import sys
+st.write(f"Running on Python {sys.version}")
